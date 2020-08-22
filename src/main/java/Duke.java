7820649127b1b49326e79ad3f46fc9757be6352a
WangZixin67 <60345019+WangZixin67@users.noreply.github.com>
2020-08-22 11:20:23 +0800

    Level-2
    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,8 +9,25 @@ public class Duke {
 
     public static void echo(String content)
     {
-        System.out.println("\n\t"+content+"\n");
+        System.out.println(content+"\n");
     }
+
+    public static void addlist(String content,String[] list,int sum)
+    {
+        System.out.print("\n\tadded: ");
+        echo(content);
+        list[sum]=content;
+    }
+
+    public static void showlist(String[] list, int sum)
+    {
+        for (int i=0;i<sum;i++)
+        {
+            System.out.println("\t"+(i+1)+". "+list[i]);
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,11 +39,21 @@ public class Duke {
         System.out.println("What can I do for you?\n");
         Scanner in=new Scanner(System.in);
         String answer;
+        String[] list=new String[100];
+        int sum=0;
         while(true)
         {
             answer=in.nextLine();
             if (answer.equals("bye")) break;
-            echo(answer);
+            else if (answer.equals("list"))
+            {
+                showlist(list,sum);
+            }
+            else
+            {
+                addlist(answer,list,sum);
+                sum++;
+            }
         }
         goodbye();
     }
