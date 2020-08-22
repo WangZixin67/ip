fecbc7278b17e90e43302ad436d6847455bef827
WangZixin67 <60345019+WangZixin67@users.noreply.github.com>
2020-08-22 11:06:33 +0800

    level-1
    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
+
 public class Duke {
+    public static void goodbye()
+    {
+        System.out.println("\nBye.Hope to see you again soon!");
+    }
+
+    public static void echo(String content)
+    {
+        System.out.println("\n\t"+content+"\n");
+    }
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,6 +20,14 @@ public class Duke {
         System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye.Hope to see you again soon!");
+        Scanner in=new Scanner(System.in);
+        String answer;
+        while(true)
+        {
+            answer=in.nextLine();
+            if (answer.equals("bye")) break;
+            echo(answer);
+        }
+        goodbye();
     }
 }
