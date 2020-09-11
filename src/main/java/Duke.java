6f184c726ed5a23c39fce360fc991a61458bbf33
WangZixin67 <60345019+WangZixin67@users.noreply.github.com>
2020-09-11 22:41:43 +0800

    Level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -107,6 +107,30 @@ public class Duke {
         System.out.println();
     }
 
+    public static void processDelete(Task[] tasks, String answer, int sum){
+        try {
+            String[] sentences = answer.split("delete ");
+            int index = Integer.parseInt(sentences[1]);
+            if (index>sum){
+                System.out.println("\n\tThe index exceed the range of tasks.\n");
+                return;
+            }
+            System.out.println("\n\tNoted. I've removed this task.");
+            System.out.println("\t  " + tasks[index - 1]);
+            for (int i = index - 1; i < sum - 1; i++) {
+                tasks[i] = tasks[i + 1];
+            }
+            tasks[sum - 1] = null;
+            System.out.println("\tNow you have " + (sum - 1) + " tasks in the list.\n");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("\n\tPlease enter an index.\n");
+        }
+        catch (NumberFormatException e){
+            System.out.println("\n\tPlease enter a number as index.\n");
+        }
+    }
+
     public static void main(String[] args) {
         greeting();
         Scanner in=new Scanner(System.in);
@@ -120,9 +144,6 @@ public class Duke {
             else if (answer.equals("list")) {
                 printList(tasks, sumList);
             }
-            else if (answer.contains("done")) {
-                processDone(answer, tasks);
-            }
             else if (answer.contains("todo")) {
                 sumList=processTodo(answer, tasks, sumList);
             }
@@ -132,8 +153,15 @@ public class Duke {
             else if (answer.contains("event")) {
                 sumList=processEvent(answer, tasks, sumList);
             }
+            else if (answer.contains("delete")){
+                processDelete(tasks,answer,sumList);
+                sumList--;
+            }
+            else if (answer.contains("done")) {
+                processDone(answer, tasks);
+            }
             else {
-                System.out.println("\n\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println("\n\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n");
             }
         }
         goodbye();
